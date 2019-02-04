f0d8723adf1baed5af88eba5109d4f755b32739c
adrioro <artistsvillage@me.com>
2019-02-04 09:10:34 -0500

    Third commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+//This will return any string that has X amount of occurences in a row.
+var myExp = /a{2}/g;
+$("#button").click(function () {
+    var test = myExp.test("My first name is Adriana");
+    alert(test);
+});
+
 //This will return true:
 var myExp = /Adriana/g;
 $("#button").click(function () {
