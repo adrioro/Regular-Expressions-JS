36505b520a6e611c3475dba81edb012e74ebf53d
adrioro <artistsvillage@me.com>
2019-02-04 09:34:15 -0500

    Fifth commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-//This will return any string that has X amount of occurences in a row.
-var myExp = /l{2}/g;
+//This will turn true because it's looking for a new line.
+var myExp = /Adriana\n/g; //This look for a new line character.
 $("#button").click(function () {
-    var test = myExp.test("My first name is Kallob");
+    var test = myExp.test("My first name is:\n Adriana");
     alert(test);
 });
 
-/*
+//This will return any string that has X amount of occurences in a row.
+var myExp = /l{2}/g;
+$("#button").click(function () {
+    var test = myExp.test("My first name is Kallob"); //Two ls in a row is true.
+    alert(test);
+});
+
 
 //This will return true:
 var myExp = /Adriana/g;
@@ -19,5 +25,3 @@ $("#button").click(function () {
     var test = myExp.test("My first name is Adriana");
     alert(test);
 });
-
-*/
